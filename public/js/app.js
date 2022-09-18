9816449046957826e081216fb0ca0566383b65d3
vintiz-dot <vintizfjes@gmail.com>
2022-09-18 16:12:31 -0400

    break

--- public/js/app.js
+++ public/js/app.js
@@ -7,7 +7,7 @@ document.getElementById("submit").addEventListener("click", function (x) {
   spin?.classList.remove("hidden");
   const address = document.querySelector("#input").value;
   document.querySelector("#input").value = "";
-  if (!address) return error();
+  // if (!address) return error();
   fetch(`/weather?address=${address}&units=metrics`).then((res) => {
     res.json().then((data) => {
       if (data.err) return displayError(data);
@@ -39,3 +39,22 @@ const displayError = function (data) {
   spin.classList.add("hidden");
   current.insertAdjacentHTML("afterbegin", `<p>${data.err}</p>`);
 };
+
+mapboxgl.accessToken =
+  "pk.eyJ1IjoidmludGl6IiwiYSI6ImNsN3E5Z2lzejAzYTQzcHNhOG14czR6aXQifQ.XuWMJW9fOdC59dGbIeR9Fg";
+const map = new mapboxgl.Map({
+  container: "map", // Specify the container ID
+  style: "mapbox://styles/mapbox/outdoors-v11", // Specify which map style to use
+  center: [-122.43877, 37.75152], // Specify the starting position [lng, lat]
+  zoom: 11.5, // Specify the starting zoom
+});
+let lng;
+let lat;
+
+map.on("click", (event) => {
+  // When the map is clicked, set the lng and lat constants
+  // equal to the lng and lat properties in the returned lngLat object.
+  lng = event.lngLat.lng;
+  lat = event.lngLat.lat;
+  console.log(`${lng}, ${lat}`);
+});
